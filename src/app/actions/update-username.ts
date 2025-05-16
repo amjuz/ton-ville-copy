27876cc7d4b2c089d912129868f005ed28f8f321
Amjuz <137517288+amjuz@users.noreply.github.com>
2025-05-16 18:26:48 +0530

    feat: update username server action

---
+++ src/app/actions/update-username.ts
@@ -0,0 +1,26 @@
+'use server'
+
+import { getServerClient } from '@/lib/supabase/server'
+
+export async function updateUsername(username: string) {
+
+//@TODO: need to do username validation here
+  const supabase = await getServerClient()
+  const {
+    data: { user },
+    error: userError,
+  } = await supabase.auth.getUser()
+
+  if (userError || !user) {
+    console.error('User fetch error:', userError?.message)
+    return
+  }
+
+  const { error } = await supabase.from('profiles').update({ username }).eq('id', user.id)
+
+  if (error) {
+    console.error('Failed to update username:', error.message)
+  } else {
+    console.log('Username updated to:', username)
+  }
+}
