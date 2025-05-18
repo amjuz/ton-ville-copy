131ec8867e44554585bb6cb213c4248c2c328510
Amjuz <137517288+amjuz@users.noreply.github.com>
2025-05-18 17:23:30 +0530

    fix: removed deprecated files

--- tests/global-teardown.ts
+++
@@ -1,9 +0,0 @@
-import { FullConfig } from '@playwright/test'
-import { cleanupTestData } from '@test/utils/database'
-
-async function globalTeardown(config: FullConfig) {
-  // Clean up test data
-  await cleanupTestData()
-}
-
-export default globalTeardown
