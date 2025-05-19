e1dcac4649257bcaa6b022ddc54e89bf7149ff5c
Amjuz <137517288+amjuz@users.noreply.github.com>
2025-05-19 15:21:44 +0530

    chore: deleted all migration files

--- supabase/migrations/20250516031708_update_profile_table.sql
+++
@@ -1,7 +0,0 @@
-alter table "public"."profiles" add column "follower_count" integer;
-
-alter table "public"."profiles" add column "following_count" integer;
-
-alter table "public"."profiles" add column "username" text;
-
-
--- supabase/migrations/20250517025550_profile_image.sql
+++
@@ -1,8 +0,0 @@
-insert into
-  storage.buckets (id, name, file_size_limit)
-values
-  ('images', 'images', 52428800);
-  
-CREATE POLICY "Give anon users access to JPG images in folder 1ffg0oo_0" ON storage.objects FOR SELECT TO public USING (bucket_id = 'images' AND storage."extension"(name) = 'jpg' AND LOWER((storage.foldername(name))[1]) = 'public' AND auth.role() = 'anon');
-
-CREATE POLICY "Give anon users access to JPG images in folder 1ffg0oo_1" ON storage.objects FOR INSERT TO public WITH CHECK (bucket_id = 'images' AND storage."extension"(name) = 'jpg' AND LOWER((storage.foldername(name))[1]) = 'public' AND auth.role() = 'anon');
