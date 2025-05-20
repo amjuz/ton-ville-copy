f1d5ff16df3d090b1ef74c1762668a513c8ad527
Amjuz <137517288+amjuz@users.noreply.github.com>
2025-05-20 23:53:21 +0530

    chore: removed and replaced old server actions

--- src/lib/auth/validate-init-data.ts
+++
@@ -1,7 +0,0 @@
-import { isValid } from '@telegram-apps/init-data-node'
-import { getEnvVariable, isDevelopment } from '@/lib/utils/env'
-
-export function validateInitData(initDataRaw: string): boolean {
-  const botToken = getEnvVariable('BOT_TOKEN')
-  return isDevelopment() ? true : isValid(initDataRaw, botToken)
-}
