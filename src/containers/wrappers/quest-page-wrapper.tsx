d7a324107864b5d5b4d513f1048d25d60991e3a9
Amjuz <137517288+amjuz@users.noreply.github.com>
2025-05-21 21:38:56 +0530

    chore: added quest page data fetching

--- src/containers/wrappers/quest-page-wrapper.tsx
+++ src/containers/wrappers/quest-page-wrapper.tsx
@@ -4,6 +4,11 @@ import React from 'react'
 import questPlaceHolders from '@/assets/images/mock/quest-card-mock.png'
 import CustomOrderedList from '@/components/Elements/custom-ordered-list'
 import { Button } from '@/components/ui/button'
+import { useParams } from 'next/navigation'
+import { getQuest } from '@/lib/supabase/quests/quests-table'
+import { useQuery } from '@tanstack/react-query'
+import { Skeleton } from '@/components/ui/skeleton'
+import { QuestCardSkeleton } from '@/components/skelton/quest-card-skeleton'
 
 const items = [
   'Retweet and Like the tweet in the link below from your twitter account.',
@@ -18,20 +23,30 @@ export default function QuestPageWrapper() {
    * fetch data here
    *  so the suspense can work
    */
+  const params = useParams()
+  const questId = params.questId as string
+
+  const { data, error, isLoading } = useQuery({
+    queryKey: ['event-page', questId],
+    queryFn: () => getQuest(questId),
+  })
+  if (!data) return <>No data found</>
+  if (error) return <>Error fetching events</>
+  if (isLoading) return <QuestCardSkeleton/>
   return (
     <div className="mt-4 p-2 sm:p-4">
       <Image
-        src={questPlaceHolders.src}
+        src={data.questImage ?? questPlaceHolders.src}
         width={720}
         height={480}
-        alt="hello"
+        alt="Quest image fallback"
         className="rounded-2xl border-2 object-cover"
       />
       <div className="mt-5 pl-1">
-        <h1 className="text-2xl font-bold">Bybit Quest</h1>
-        <p className="text-muted-foreground">Valve Protocol</p>
+        <h1 className="text-2xl font-bold">{data.title}</h1>
+        <p className="text-muted-foreground">{data.subTitle}</p>
         <p className="mt-2">
-          Unlock crypto wisdom and stand to win 25 USDT each week by 3 simple steps!
+          {data.description}
         </p>
       </div>
       <div className="mt-8 flex gap-2">
